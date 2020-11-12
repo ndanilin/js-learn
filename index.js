a8644889f6f0a49dea70f8be3338af68a90afd32
ndanilin <ndanilin87@gmail.com>
2020-11-12 22:05:10 +0300

    add files

---
+++ index.js
@@ -0,0 +1,54 @@
+var el_up = document.getElementById("GFG_UP");
+		
+		var list = JSON.parse(data2); 
+		
+		el_up.innerHTML = "Click on the button to create the " 
+				+ "table from the JSON data.<br><br>"; 
+		
+		function GFG_FUN() { 
+			var cols = []; 
+			
+			for (var i = 0; i < list.length; i++) { 
+				for (var k in list[i]) { 
+					if (cols.indexOf(k) === -1) { 
+						
+						// Push all keys to the array 
+						cols.push(k); 
+					} 
+				} 
+			} 
+			
+			// Create a table element 
+			var table = document.createElement("table"); 
+			
+			// Create table row tr element of a table 
+			var tr = table.insertRow(-1); 
+			
+			for (var i = 0; i < cols.length; i++) { 
+				
+				// Create the table header th element 
+				var theader = document.createElement("th"); 
+				theader.innerHTML = cols[i]; 
+				
+				// Append columnName to the table row 
+				tr.appendChild(theader); 
+			} 
+			
+			// Adding the data to the table 
+			for (var i = 0; i < list.length; i++) { 
+				
+				// Create a new row 
+				trow = table.insertRow(-1); 
+				for (var j = 0; j < cols.length; j++) { 
+					var cell = trow.insertCell(-1); 
+					
+					// Inserting the cell at particular place 
+					cell.innerHTML = list[i][cols[j]]; 
+				} 
+			} 
+			
+			// Add the newely created table containing json data 
+			var el = document.getElementById("table"); 
+			el.innerHTML = ""; 
+			el.appendChild(table); 
+		}	 
